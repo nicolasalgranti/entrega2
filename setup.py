48854ac1be2f6910ff19e3bb2479b0f5e4e5fa92
nicolasalgranti <nicolasalgranti@gmail.com>
2023-12-21 18:43:31 -0300

    entrega2-correccion.2

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup (
     version="1.0",
     author="Nicolas Algranti",
     author_email="nicolasalgranti@gmail.com",
-
     packages=["paquete1"],
+    py_modules=["paquete1.modulo1", "paquete1.modulo2"]
 )
